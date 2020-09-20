27580daa125a0cfb1516bb91c579aa58df80cf40
yyf <yyfjlyx@163.com>
2020-09-20 14:13:55 +0800

    第一次提交代码

---
+++ syntax/src/main/java/com/technology/inter/Main.java
@@ -0,0 +1,7 @@
+package com.technology.inter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ syntax/src/main/java/com/technology/inter/Student.java
@@ -0,0 +1,8 @@
+package com.technology.inter;
+
+public interface Student {
+    int AGE = 10;
+    String NAME = "cot";
+
+    void setAge(int age);
+}
