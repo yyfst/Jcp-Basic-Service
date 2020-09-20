400ddefc58545f9e56d0699362d83a23e242e381
yyf <yyfjlyx@163.com>
2020-09-20 23:44:03 +0800

    实现Compare接口

--- syntax/src/main/java/com/technology/inter/Student.java
+++ syntax/src/main/java/com/technology/inter/Student.java
@@ -1,8 +1,21 @@
 package com.technology.inter;
 
+/**
+ * 1.接口中的字段默认是static final，可以省略不写.
+ * 2.接口中的方法默认是public，可以省略不写.
+ */
 public interface Student {
+    /**
+     * 学生年龄.
+     */
     int AGE = 10;
+    /**
+     * 学生姓名.
+     */
     String NAME = "cot";
-
+    /**
+     *
+     * @param age 年龄
+     */
     void setAge(int age);
 }
