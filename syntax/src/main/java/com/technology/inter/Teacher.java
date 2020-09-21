a36184647087e7e8101bd0d5893b89a365eba1a0
yyf <yyfjlyx@163.com>
2020-09-22 00:04:29 +0800

    新增Teacher接口，演示默认方法冲突

---
+++ syntax/src/main/java/com/technology/inter/Teacher.java
@@ -0,0 +1,12 @@
+package com.technology.inter;
+
+/**
+ * 该类主要用于演示接口中的方法发生冲突时的解决方式
+ */
+public interface Teacher {
+    public void setAge(int age);
+
+    default public void defaultMethod() {
+        System.out.println("this is a default method in teacher");
+    }
+}
