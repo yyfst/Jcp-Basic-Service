0326939ba4e54d9c82c2753c8b3074a97f085d08
yyf <yyfjlyx@163.com>
2020-10-05 19:59:55 +0800

    实现Cloneable完成对象的深拷贝

---
+++ syntax/src/main/java/com/technology/interface_java/CloneableImp.java
@@ -0,0 +1,46 @@
+package com.technology.interface_java;
+
+//Cloneable其实是个空接口，没有任何方法，实现该接口的目的是为了能够使用instanceof
+public class CloneableImp implements Cloneable {
+    private int id;
+    private String address;
+    private StudentImpl student;
+
+    public CloneableImp(int id, String address) {
+        this.id = id;
+        this.address = address;
+    }
+
+    public StudentImpl getStudent() {
+        return student;
+    }
+
+    public CloneableImp(int id, String address, StudentImpl student) {
+        this.id = id;
+        this.address = address;
+        this.student = student;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    protected CloneableImp clone() throws CloneNotSupportedException {
+        CloneableImp cloned = (CloneableImp) super.clone();
+        cloned.student = student.clone();
+        return cloned;
+    }
+}
