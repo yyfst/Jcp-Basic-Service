a9e1e96221f3bc3bd41191390b213c556e25e582
yyf <yyfjlyx@163.com>
2020-10-07 14:16:19 +0800

    新增Log4j日志

--- syntax/src/main/java/com/technology/interface_java/ActionListenerImpl.java
+++ syntax/src/main/java/com/technology/interface_java/ActionListenerImpl.java
@@ -1,16 +1,20 @@
 package com.technology.interface_java;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.Instant;
 
 //该接口为动作监听器，用于定义定时器，传给Timer类
 public class ActionListenerImpl implements ActionListener {
-    public static int count = 0;
+    private static final Logger log = LoggerFactory.getLogger(ActionListenerImpl.class);
+    public static int count = 1;
 
     @Override
     public void actionPerformed(ActionEvent e) {
         count++;
-        System.out.println(Instant.ofEpochMilli(e.getWhen()) + "\tcount=\t" + count);
+        log.info("count={}", count);
     }
 }
--- syntax/src/main/java/com/technology/interface_java/Main.java
+++ syntax/src/main/java/com/technology/interface_java/Main.java
@@ -4,7 +4,13 @@ import java.time.Clock;
 import java.util.*;
 import javax.swing.Timer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         //创建List集合
         List<ComparableImp> list = new ArrayList<>();
@@ -20,10 +26,10 @@ public class Main {
 
         //遍历集合，var是自动类型推断
         for (var data : list) {
-            System.out.println(data.getName() + " " + data.getAge() + " " + data.getSalary());
+//            log.info("data.getName()={},data.getAge()={},data.getSalary()={}",
+//                    data.getName(),data.getAge(),data.getSalary());
+            log.info("name:" + data.getName() + " age:" + data.getAge() + " salary:" + data.getSalary());
         }
-
-        System.out.println("==========================");
         List<ComparableImp> comparatorImps = new ArrayList<>();
         comparatorImps.add(new ComparableImp("tom", 1000.0, 24));
         comparatorImps.add(new ComparableImp("Jim", 2090.0, 34));
@@ -32,8 +38,9 @@ public class Main {
         comparatorImps.add(new ComparableImp("Hoe", 3200.0, 27));
         //使用比较器进行排序比较
         comparatorImps.sort(new ComparatorImp());
+        log.info("==============================");
         for (var data : comparatorImps) {
-            System.out.println(data.getName() + " " + data.getAge());
+            log.info("name:" + data.getName() + " age:" + data.getAge() + " salary:" + data.getSalary());
         }
 
         //测试字符串浅拷贝后，是否会改变原来的值
@@ -47,19 +54,19 @@ public class Main {
 
     //测试字符串浅拷贝后，修改拷贝后的变量是否会影响原来的变量
     private static void stringCopy() {
-        System.out.println("==========================");
+        log.info("==============================");
         String string = new String("stringCopy");
         String copy = string;
-        System.out.println("init value: " + string + "\t" + copy);
+        log.info("init value:" + string + " " + copy);
 
         //修改copy的值，将其转换为小写，查看string的值是否发生改变
         copy = copy.toLowerCase();
-        System.out.println("moidfied value: " + string + "\t" + copy);
+        log.info("moidfied value:" + string + " " + copy);
     }
 
     //测试clone拷贝
     private static void cloneTest() {
-        System.out.println("==========================");
+        log.info("==============================");
         //没有使用Student类
 //        CloneableImp cloneableImp = new CloneableImp(101, "hunan");
 //        CloneableImp cloneableImpCopy = null;
@@ -75,7 +82,7 @@ public class Main {
 //        System.out.println(cloneableImpCopy.getId() + "\t" + cloneableImpCopy.getAddress());
 
         //使用Student类
-        System.out.println("==========================");
+        log.info("==============================");
         StudentImpl student = new StudentImpl();
         student.setAge(19);
         CloneableImp deepCopy1 = new CloneableImp(102, "shanxi", student);
@@ -86,26 +93,31 @@ public class Main {
             e.printStackTrace();
         }
         //打印修改student之前的值
-        System.out.println(deepCopy1.getStudent().getAge());
-        System.out.println(deepCopy2.getStudent().getAge());
+        log.info("deepCopy1.age:" + deepCopy1.getStudent().getAge());
+        log.info("deepCopy2.age:" + deepCopy2.getStudent().getAge());
 
         deepCopy2.getStudent().setAge(28);
 
         //打印修改student之后的值，deepCopy1与deepCopy2中student的age均改变
-        System.out.println(deepCopy1.getStudent().getAge());
-        System.out.println(deepCopy2.getStudent().getAge());
+        log.info("deepCopy1.age:" + deepCopy1.getStudent().getAge());
+        log.info("deepCopy2.age:" + deepCopy2.getStudent().getAge());
         //这两个student没有经过深拷贝，共享了一个对象内存，所以是同一个对象
-        System.out.println(deepCopy1.getStudent() == deepCopy2.getStudent());
+        boolean res = deepCopy1.getStudent() == deepCopy2.getStudent();
+        log.info(String.valueOf(res));
 
     }
 
     private static void timer() {
         ActionListenerImpl listener = new ActionListenerImpl();
-        Timer timer = new Timer(1000,listener);
+        Timer timer = new Timer(1000, listener);
         timer.start();
+
         //让程序持续运行，从而看到定时器的作用
-        while (true){
-            ;
+        while (true) {
+//            if ((ActionListenerImpl.count % 10) == 0) {
+//                LOGGER.error("error occurred...");
+//            }
+//            LOGGER.info("count in main is:{}", ActionListenerImpl.count);
         }
     }
 
